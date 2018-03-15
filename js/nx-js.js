975ace83b3a6aded1c336c0699cc3ae047ee2cf2
niczhou <madeozhou@sina.com>
2018-03-15 17:04:34 +0800

    todo js

--- js/nx-js.js
+++ js/nx-js.js
@@ -1,7 +1,18 @@
 ﻿$(document).ready(function(){
-	tetris();
+	alert('stt');
+	// tetris();
+	// toggleTab();
 });
 
 	function tetris(){
 		// $('.nx-wt-item:last-child').css('background','blue');
+	}
+	
+	function toggleTab(){
+		$('.nx-about-tab:first').hover(function(){
+			$('.nx-about-tab:first').css('background','#ccc');
+		};
+		$('.nx-about').hover(function(){
+			alert('daf');
+		});
 	}
